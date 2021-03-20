1c4625a801e3b277ccbce22c5a432ff98ed1a19c
Lee Sohyeong <69043819+sohyeong-dev@users.noreply.github.com>
2021-03-20 12:52:22 +0900

    mobile 버전 (#26)
    - main
    - login / signup
    - 업체등록하기
    - 카페 목록 페이지
    - 카페 상세 페이지

--- posts/static/posts/js/main.js
+++ posts/static/posts/js/main.js
@@ -1,5 +1,5 @@
 let isProcessing = false;
-function drawCafeItem(num = 0) {
+function drawCafeItem(num = 0, offset = 4) {
     return new Promise(function(resolve, reject) {
         if(num < 1) {
             reject();
@@ -8,45 +8,46 @@ function drawCafeItem(num = 0) {
                 isProcessing = true;
                 $.ajax({
                     url: "/rcmd",
-                    data: { num },
+                    data: { num, offset },
                     type: 'get',
                     dataType: 'json',
                     success: function(data) {
                         // console.log(data);
                         if(data.result) {
-                            const cafe = data.cafe[0];
-                            // console.log(cafe);
                             let str = '';
-                            str += '<li class="cafe-item">';
-                            str += '<a href="' + cafe['id'] + '" class="cafe">';
-                            str += '<div class="cafe-content">';
-                            str += '<div class="cafe-img">';
-                            str += '<img id="' + cafe['id'] + '" src="/static/posts/images/logo-2.png">';
-                            $.ajax({
-                                url: "/image",
-                                data: { "cafe": cafe['id'] },
-                                type: 'get',
-                                dataType: 'json',
-                                success: function(data) {
-                                    if(data.image) {
-                                        $('#' + cafe['id']).attr('src', data.image);
-                                    } else {
-                                        $('#' + cafe['id']).attr('src', '/static/posts/images/logo-2.png');
+                            data.cafes.forEach(cafe => {
+                                // console.log(cafe);
+                                str += '<li class="cafe-item">';
+                                str += '<a href="' + cafe['id'] + '" class="cafe">';
+                                str += '<div class="cafe-content">';
+                                str += '<div class="cafe-img">';
+                                str += '<img id="' + cafe['id'] + '" src="/static/posts/images/logo-2.png">';
+                                $.ajax({
+                                    url: "/image",
+                                    data: { "cafe": cafe['id'] },
+                                    type: 'get',
+                                    dataType: 'json',
+                                    success: function(data) {
+                                        if(data.image) {
+                                            $('#' + cafe['id']).attr('src', data.image);
+                                        } else {
+                                            $('#' + cafe['id']).attr('src', '/static/posts/images/logo-2.png');
+                                        }
                                     }
-                                }
-                            })
-                            str += '</div>';
-                            str += '<div class="cafe-text">';
-                            str += '<div class="info">';
-                            str += '<span>' + cafe['name'] + '</span>';
-                            str += '</div>';
-                            str += '<div class="etc">';
-                            str += cafe['address'];
-                            str += '</div>';
-                            str += '</div>';
-                            str += '</div>';
-                            str += '</a>';
-                            str += '</li>';
+                                })
+                                str += '</div>';
+                                str += '<div class="cafe-text">';
+                                str += '<div class="info">';
+                                str += '<span>' + cafe['name'] + '</span>';
+                                str += '</div>';
+                                str += '<div class="etc">';
+                                str += cafe['address'];
+                                str += '</div>';
+                                str += '</div>';
+                                str += '</div>';
+                                str += '</a>';
+                                str += '</li>';
+                            });
                             resolve(str);
                         } else {
                             reject('end');
@@ -86,40 +87,38 @@ $(document).ready(function() {
 
     $('.page-btn button').click(async function() {
         if($(this).hasClass('prev-btn')) {
-            if(num < 2) {
+            if(num <= offset) {
                 return;
             }
             try {
-                const result = await drawCafeItem(num - 1);
+                const result = await drawCafeItem(num - offset, offset);
                 if(result !== null) {
-                    $('.cafe-item').last().remove();
-                    $('.cafe-list').prepend(result);
-                    num -= 1;
+                    $('.cafe-list').html(result);
+                    num -= offset;
                 }
-                if($('.next-btn').css('display') === 'none') {
-                    $('.next-btn').css('display', 'block');
-                }
+                // if($('.next-btn').css('display') === 'none') {
+                //     $('.next-btn').css('display', 'block');
+                // }
             } catch (err) {}
         } else {
             try {
-                const result = await drawCafeItem(num + offset);
+                const result = await drawCafeItem(num + offset, offset);
                 if(result !== null) {
-                    $('.cafe-item').first().remove();
-                    $('.cafe-list').append(result);
-                    num += 1;
+                    $('.cafe-list').html(result);
+                    num += offset;
                 }
             } catch (err) {
                 // console.error(err);
-                if(err === 'end') {
-                    $('.next-btn').css('display', 'none');
-                }
+                // if(err === 'end') {
+                //     $('.next-btn').css('display', 'none');
+                // }
             }
         }
-        if(num > 1 && $('.prev-btn').css('display') === 'none') {
-            $('.prev-btn').css('display', 'block');
-        } else if (num < 2 && $('.prev-btn').css('display') === 'block') {
-            $('.prev-btn').css('display', 'none');
-        }
+        // if(num > 1 && $('.prev-btn').css('display') === 'none') {
+        //     $('.prev-btn').css('display', 'block');
+        // } else if (num < 2 && $('.prev-btn').css('display') === 'block') {
+        //     $('.prev-btn').css('display', 'none');
+        // }
         // console.log(num);
     });
 });
