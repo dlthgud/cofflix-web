d81e0205c05ce736fda86c5e339f522f3fa9e6ed
Lee Sohyeong <69043819+sohyeong-dev@users.noreply.github.com>
2021-05-04 23:58:18 +0900

    Feature/layout (#31)
    * mypage test commit
    
    * git commit test again
    
    * mypage fix
    
    * like update
    
    * like complete yet
    
    * fix: main text
    
    * fix: detail layout
    
    * fix: scroll button
    
    * [feat] menu and detail page (#28)
    
    * feat: bottom menu, fancy box, swiper, map link
    
    Co-authored-by: sohyeong-dev <so-hyeong@naver.com>
    
    * fix: kakao map app key
    
    * fix: web swiper image size
    
    * 마이페이지 (#29)
    
    * mypage test commit
    
    * git commit test again
    
    * mypage fix
    
    Co-authored-by: kanghyunki <kanghhkk@gmail.com>
    
    * fix: swiper in web
    
    * add: like icon image
    
    * cafe list upgrade
    
    * fix: mypage
    
    * fix: login check
    
    * fix: header profile box layout
    
    * remove ios default style
    
    Co-authored-by: kanghyunki <kanghhkk@gmail.com>
    Co-authored-by: sohyeong-dev <so-hyeong@naver.com>
    
    😨

--- posts/static/posts/js/lists.js
+++ posts/static/posts/js/lists.js
@@ -1,71 +1,129 @@
 $(document).ready(function() {
+
+    var loading = false;
+    
+    var page = 2;
+    var offset = 4;
+    
     let url = decodeURI(decodeURIComponent(location.href));
     let param = url.split('?')[1];
-    let keyword = param.split('=')[1];
-    $('.tag-box input[value="'+keyword+'"]').addClass('active');
-    
+    if (typeof param !== 'undefined') {
+        let keyword = param.split('=')[1];
+        $('.tag-box input[value="'+keyword+'"]').addClass('active');
+    }
+        
     $('.all').click(function() {
        $('.tag-box input').removeClass('active');
+        
+        page = 1;
         
         getContent();
     });
 
     $('.tag-box input').click(function() {
         $(this).toggleClass('active');
+        
+        page = 1;
 
         getContent();
     });
-});
 
-function getContent() {
-    let keywords = [];
-    $('.tag-box input.active').each(function() {
-        keywords.push($(this).val());
+    $(window).on('scroll', function() {        
+        var scrollHeight = $(document).height();
+        var scrollPosition = $(window).height() + $(window).scrollTop();
+
+        if ((scrollPosition > scrollHeight - 500) && loading == false) {
+            loading = true;
+
+            getContent(true);
+        }
+    });
+    
+    $(document).on('click', '.action-btn', function(event) {
+        event.preventDefault();
+        
+        const cafeId = $(this).parent().data('cafe');
+        if ($(this).hasClass('like-btn')) {
+            location.replace("/like/" + cafeId + "/?next=" + location.href);
+        } else if ($(this).hasClass('mark-btn')) {
+            location.replace("/mark/" + cafeId + "/?next=" + location.href);
+        }
+        
     });
 
-    $.ajax({
-        // url: "{% url 'posts:main' %}",
-        url: "/lists",
-        data: { "keywords": keywords },
-        type: 'get',
-        dataType: 'json',
-        success: function(data) {
-            var str = '';
-            // console.log(data);
-            $.each(data.cafes, function(idx, cafe) {
-                // console.log(cafe);
-                str += '<li class="filter-item">';
-                str += '<a href="/' + cafe['id'] + '" class="filter">';
-                str += '<div class="filter-content">';
-                str += '<div class="filter-img">';
-                str += '<img id="' + cafe['id'] + '" src="' + cafe['img'] + '">';
-                $.ajax({
-                    url: "/image",
-                    data: { "cafe": cafe['id'] },
-                    type: 'get',
-                    dataType: 'json',
-                    success: function(data) {
-                        if(data.image) {
-                            $('#' + cafe['id']).attr('src', data.image);
-                        } else {
-                            $('#' + cafe['id']).attr('src', '/static/posts/images/logo-2.png');
+    function getContent(scroll = false) {
+
+        let keywords = [];
+        $('.tag-box input.active').each(function() {
+            keywords.push($(this).val());
+        });
+
+        $.ajax({
+            // url: "{% url 'posts:main' %}",
+            url: "/lists",
+            data: {
+                "keywords": keywords,
+                "page": page,
+                "offset": offset,
+            },
+            type: 'get',
+            dataType: 'json',
+            success: function(data) {
+                var str = '';
+                // console.log(data);
+                if (data.cafes) {
+                    page++;
+                }
+                loading = false;
+                
+                if (!scroll) {
+                    $('.filter-list').html('');
+                }
+                
+                $.each(data.cafes, function(idx, cafe) {
+                    // console.log(cafe);
+                    
+                    let $item = $(".filter-item.template").clone();
+                    $item.removeClass('template');
+                    $item.find('.filter').attr("href", "/" + cafe['id']);
+                    $item.find('.swiper-wrapper').attr("id", cafe['id']);
+                    
+                    $.ajax({
+                        url: "/image",
+                        data: { "cafe": cafe['id'] },
+                        type: 'get',
+                        dataType: 'json',
+                        success: function(data) {
+                            // console.log(data);
+                            if(data.images) {
+                                data.images.forEach(image => {
+                                    let slide = '<div class="swiper-slide">';
+                                    slide += '<img src="' + image + '">';
+                                    slide += '</div>';
+                                    $('#' + cafe['id']).append(slide);
+                                });
+                            } else {
+                                let slide = '<div class="swiper-slide">';
+                                    slide += '<img src="/static/posts/images/logo-2.png">';
+                                    slide += '</div>';
+                            }
                         }
+                    })
+                    
+                    $item.find('.btn-wrap').attr('data-cafe', cafe['id']);
+                    if (cafe.liked_users.includes(data.user)) {
+                        $item.find('.like-btn img').addClass('active');
                     }
-                })
-                str += '</div>';
-                str += '<div class="filter-text">';
-                str += '<div class="info">';
-                str += '<span>' + cafe['name'] + '</span>';
-                str += '</div>';
-                str += '<div class="etc">';
-                str += cafe['address'];
-                str += '</div>';
-                str += '</div>';
-                str += '</div>';
-                str += '</a>';
-                str += '</li>';
-            });
-            $('.filter-list').html(str);
-        },
-    });
-}
+                    if (cafe.marked_users.includes(data.user)) {
+                        $item.find('.mark-btn img').addClass('active');
+                    }
+                    
+                    $item.find('.info span').text(cafe['name']);
+                    $item.find('.etc').text(cafe['address']);
+                    
+                    $('.filter-list').append($item);
+                });
+            },
+        });
+    }
+});
